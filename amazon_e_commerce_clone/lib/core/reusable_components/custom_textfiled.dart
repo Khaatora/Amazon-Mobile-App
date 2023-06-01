cabb7838ea2faab58058db01c33d4ec823c60a90
Khaatora <ahosari20@gmail.com>
2023-06-01 06:42:09 +0300

    custom textfiled

---
+++ amazon_e_commerce_clone/lib/core/reusable_components/custom_textfiled.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    this.style,
+    required this.controller,
+    this.decoration,
+    required this.hintText,
+    required this.validator,
+    this.textInputType,
+    this.obscureText = false,
+    this.autocorrect = true,
+    this.enableSuggestions = true,
+    this.readOnly = false,
+    this.minLines,
+    this.maxLines = 1,
+  }) : super(key: key);
+
+  final TextStyle? style;
+  final TextEditingController controller;
+  final InputDecoration? decoration;
+  final String hintText;
+  final String? Function(String?) validator;
+  final TextInputType? textInputType;
+  final bool obscureText;
+  final bool autocorrect;
+  final bool enableSuggestions;
+  final bool readOnly;
+  final int? minLines;
+  final int? maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: style,
+      controller: controller,
+      decoration: decoration ??
+           InputDecoration(
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black38)),
+            enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black38)),
+            hintText: hintText,
+          ),
+      validator: validator,
+      keyboardType: textInputType ?? TextInputType.name,
+      obscureText: obscureText,
+      autocorrect: autocorrect,
+      minLines: minLines,
+      maxLines: maxLines,
+      enableSuggestions: enableSuggestions,
+      readOnly: readOnly,
+    );
+  }
+}
