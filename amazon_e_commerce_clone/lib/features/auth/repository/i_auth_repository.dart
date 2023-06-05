b79e06768c608e469881ea4d07ef01a2230fb083
Khaatora <ahosari20@gmail.com>
2023-06-06 02:26:09 +0300

    base auth repo with login & signup params

---
+++ amazon_e_commerce_clone/lib/features/auth/repository/i_auth_repository.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../core/errors/failures/IFailures.dart';
+import '../models/login/login_response.dart';
+import '../models/signup/signup_response.dart';
+
+abstract class IAuthRepository{
+  Future<Either<IFailure, LoginResponse>> login(LoginParams params);
+
+  Future<Either<IFailure, SignupResponse>> signup(SignupParams params);
+}
+
+class SignupParams {
+  final String email;
+  final String name;
+  final String password;
+
+  SignupParams({required this.email, required this.name, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "email": email,
+      "name": name,
+      "password": password,
+    };
+  }
+}
+
+class LoginParams {
+  final String email;
+
+  Map<String, dynamic> toJson() {
+    return {
+      "email": email,
+      "password": password,
+    };
+  }
+
+  final String password;
+
+  LoginParams({required this.email, required this.password});
+}
