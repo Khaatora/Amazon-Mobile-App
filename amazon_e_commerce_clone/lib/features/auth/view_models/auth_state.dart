4c3026b52b734eb09ad68d44ffff80e83ebb823b
Khaatora <ahosari20@gmail.com>
2023-06-06 02:20:51 +0300

    added loadingState

--- amazon_e_commerce_clone/lib/features/auth/view_models/auth_state.dart
+++ amazon_e_commerce_clone/lib/features/auth/view_models/auth_state.dart
@@ -1,22 +1,32 @@
 part of 'auth_cubit.dart';
 
+enum Auth{
+  signin,
+  signup,
+}
+
 class AuthState extends Equatable {
   final Auth authType;
+  final LoadingState loadingState;
 
   const AuthState({
     this.authType = Auth.signin,
+    this.loadingState = LoadingState.init,
   });
 
   AuthState copyWith({
     Auth? authType,
+    LoadingState? loadingState,
   }) {
     return AuthState(
       authType: authType ?? this.authType,
+      loadingState: loadingState ?? this.loadingState,
     );
   }
 
   @override
   List<Object> get props => [
         authType,
+        loadingState,
       ];
 }
