91050f3c7aad0f109fdfd295e384f97798ae2098
Khaatora <ahosari20@gmail.com>
2023-06-06 02:22:31 +0300

    added Dio and Auth classes

--- amazon_e_commerce_clone/lib/core/services/services_locator.dart
+++ amazon_e_commerce_clone/lib/core/services/services_locator.dart
@@ -1,4 +1,9 @@
+import 'package:amazon_e_commerce_clone/core/constants/api_constants.dart';
+import 'package:amazon_e_commerce_clone/features/auth/models/remote_data_source.dart';
+import 'package:amazon_e_commerce_clone/features/auth/repository/auth_repository_impl.dart';
+import 'package:amazon_e_commerce_clone/features/auth/repository/i_auth_repository.dart';
 import 'package:amazon_e_commerce_clone/features/auth/view_models/auth_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -7,8 +12,24 @@ class ServicesLocator{
 
   void init(){
 
-    // Bloc/Cubit
-    sl.registerFactory<AuthCubit>(() => AuthCubit());
+    // Dio
+    sl.registerLazySingleton<Dio>(() => Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        contentType: "application/json; charset=UTF-8",
+        connectTimeout: const Duration(seconds: 20),
+        sendTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20),
+      ),
+    ));
 
+    // Bloc/Cubit
+    sl.registerFactory<AuthCubit>(() => AuthCubit(sl()));
+
+    // Repository
+    sl.registerLazySingleton<IAuthRepository>(() => AuthRepositoryImpl(sl()));
+
+    // Data Source
+    sl.registerLazySingleton<RemoteDataSource>(() => APIRemoteDataSource());
   }
 }
