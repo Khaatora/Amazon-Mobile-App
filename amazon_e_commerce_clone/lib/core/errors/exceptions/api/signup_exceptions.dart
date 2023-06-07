2fe706d343dddb3a37b687cbb541d999f1947a5f
Khaatora <ahosari20@gmail.com>
2023-06-07 04:05:03 +0300

    added exceptions

--- amazon_e_commerce_clone/lib/core/errors/exceptions/api/signup_exceptions.dart
+++ amazon_e_commerce_clone/lib/core/errors/exceptions/api/signup_exceptions.dart
@@ -4,12 +4,3 @@ class GenericAPIException extends ServerException{
 
   const GenericAPIException([super.message = "something went wrong"]);
 }
-
-class InvalidCredentialsException extends ServerException{
-  // create your own Exception and extend this class if you want to throw an Exception
-  const InvalidCredentialsException([super.message = "invalid credentials"]);
-}
-class InternalServerException extends ServerException{
-  // create your own Exception and extend this class if you want to throw an Exception
-  const InternalServerException([super.message = "Internal Server Error"]);
-}
