2cb06c0ef1bdcb7ee929a0c24f0dd7f39357da0d
Khaatora <ahosari20@gmail.com>
2023-06-07 04:11:34 +0300

    added exceptions

--- amazon_e_commerce_clone/lib/core/errors/exceptions/api/login_exceptions.dart
+++ amazon_e_commerce_clone/lib/core/errors/exceptions/api/login_exceptions.dart
@@ -5,11 +5,3 @@ class GenericAPIException extends ServerException{
 
   const GenericAPIException([super.message = "something went wrong"]);
 }
-class InvalidCredentialsException extends ServerException{
-  // create your own Exception and extend this class if you want to throw an Exception
-  const InvalidCredentialsException([super.message = "invalid credentials"]);
-}
-class InternalServerException extends ServerException{
-  // create your own Exception and extend this class if you want to throw an Exception
-  const InternalServerException([super.message = "Internal Server Error"]);
-}
--- amazon_e_commerce_clone/lib/core/errors/exceptions/server_exception.dart
+++ amazon_e_commerce_clone/lib/core/errors/exceptions/server_exception.dart
@@ -3,3 +3,13 @@ class ServerException implements Exception{
   // create your own Exception and extend this class if you want to throw an Exception
   const ServerException([this.message = "something went wrong"]);
 }
+
+class InvalidCredentialsException extends ServerException{
+  const InvalidCredentialsException([super.message = "invalid credentials"]);
+}
+class InternalServerException extends ServerException{
+  const InternalServerException([super.message = "Internal Server Error"]);
+}
+class InvalidTokenException extends ServerException{
+  const InvalidTokenException([super.message = "Invalid Token"]);
+}
