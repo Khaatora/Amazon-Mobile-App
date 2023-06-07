31b37797b30b931378d76b88bddbdfd569cfbd62
Khaatora <ahosari20@gmail.com>
2023-06-07 04:11:19 +0300

    added global keys file

---
+++ amazon_e_commerce_clone/lib/core/constants/keys.dart
@@ -0,0 +1,13 @@
+class ApiKeys{
+  static const String id = "_id";
+  static const String email = "email";
+  static const String password = "password";
+  static const String address = "address";
+  static const String type = "type";
+  static const String name = "name";
+  static const String token = "x-auth-token";
+}
+
+class SharedPrefsKeys{
+
+}
