3481685c0d4fd5ab36b46329a5b1091d9f6dbe83
Khaatora <ahosari20@gmail.com>
2023-06-15 01:41:20 +0300

    updated routes and added addProducts route

--- amazon_e_commerce_clone/lib/core/constants/app_routes.dart
+++ amazon_e_commerce_clone/lib/core/constants/app_routes.dart
@@ -1,23 +1,30 @@
+import 'package:amazon_e_commerce_clone/features/admin/posts/view/components/add_product_screen.dart';
 import 'package:amazon_e_commerce_clone/features/auth/views/auth_screen.dart';
-import 'package:amazon_e_commerce_clone/features/home/views/home_screen.dart';
 import 'package:flutter/material.dart';
+
+import '../features/home/views/admin_bottom_nav_bar_layout.dart';
+import '../features/home/views/user_bottom_nar_bar_layout.dart';
 
 class AppRoutes{
   
   //authentication screen
-  static const String authScreen ='/auth_screen';
-  static const String homeScreen ='/home_screen';
+  static const String authScreen ='/auth';
+  static const String userScreen ='/user_home';
+  static const String adminScreen ='/admin_home';
+  static const String addProductsScreen ='/add_products';
 
-  static Route<dynamic> generateRoute(RouteSettings routeSettings){
+  static Route<dynamic>? generateRoute(RouteSettings routeSettings){
     switch(routeSettings.name){
       case authScreen:
         return MaterialPageRoute(settings: routeSettings,builder: (context) => const AuthScreen(),);
-      case homeScreen:
-        return MaterialPageRoute(settings: routeSettings,builder: (context) => const HomeScreen(),);
+      case userScreen:
+        return MaterialPageRoute(settings: routeSettings,builder: (context) => const UserBottomNavBarLayout(),);
+      case adminScreen:
+        return MaterialPageRoute(settings: routeSettings,builder: (context) => const AdminBottomNavBarLayout(),);
+      case addProductsScreen:
+        return MaterialPageRoute(settings: routeSettings,builder: (context) => const AddProductScreen(),);
       default:
-        return MaterialPageRoute(builder: (context) => const Scaffold(body:  Center(
-          child: Text("Screen Does Not Exist!"),
-        ),),);
+        return null;
     }
   }
 }
