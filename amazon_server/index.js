9fc2d9f1812f309b395f1ee5c92a0999f6f4874d
Khaatora <ahosari20@gmail.com>
2023-06-17 00:24:46 +0300

    back-end

--- amazon_server/index.js
+++ amazon_server/index.js
@@ -1,2 +1,29 @@
-console.log("Hello, World")
-// didn't add http package
+//imports from packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+//imports from other files
+const authRouter = require('./routes/auth');
+
+//initializations
+const PORT = 3000;
+const app = express();
+const DBUrl = "DBUrl";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+//connections
+mongoose.connect(DBUrl).then(
+    ()=>{
+        console.log('Connection Successful');
+    }
+).catch((e)=>{
+    console.log(e);
+});
+
+//start server
+app.listen(PORT, ip, () =>{
+    console.log(`connected at port ${PORT}`);
+});
